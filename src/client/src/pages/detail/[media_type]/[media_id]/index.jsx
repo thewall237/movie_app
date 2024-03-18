c1d931b8afe6dec4ddca27b14c2b12a097157d2a
thewall237 <outsidethewall237@gmail.com>
2024-03-18 23:54:01 +0900

    ⚡️ レビュー投稿機能を追加。作品の評価点表示は途中

--- src/client/src/pages/detail/[media_type]/[media_id]/index.jsx
+++ src/client/src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -1,11 +1,80 @@
 import AppLayout from '@/components/Layouts/AppLayout';
-import { Box, Container, Grid, Typography } from '@mui/material';
+import laravelAxios from '@/lib/laravelAxios';
+import { Box, Button, Card, CardContent, Container, Fab, Grid, Modal, Rating, TextareaAutosize, Tooltip, Typography } from '@mui/material';
 import axios from 'axios';
 import Head from 'next/head';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AddIcon from '@mui/icons-material/Add'
+import StarIcon from '@mui/icons-material/Star'
 
-const detail = ({ detail, media_type }) => {
-  console.log(detail);
+const detail = ({ detail, media_type, media_id }) => {
+  const [open, setOpen] = useState(false);
+  const [rating, setRating] =  useState(0);
+  const [review, setReview] = useState("");
+  const [reviews, setReviews] = useState([]);
+  
+  const handleOpen = () => {
+    setOpen(true);
+  }
+  
+  const handleClose = () => {
+    setOpen(false);
+  }
+  
+  const handleReviewChange = (e) => {
+    setReview(e.target.value);
+  }
+  
+  const handleRatingChange = (e, newValue) => {
+    setRating(newValue);
+  }
+  
+  const isDisabled = !rating || !review.trim();
+  
+  const handleReviewAdd = async() => {
+    handleClose();
+    try {
+      const response = await laravelAxios.post(`api/reviews`, {
+        content: review,
+        rating: rating,
+        media_type: media_type,
+        media_id: media_id,
+      });
+      const newReview = response.data;
+
+      setReviews([...reviews, newReview]);
+
+      setReview("");
+      setRating(0);
+      const updatedReviews = [...reviews, newReview];
+      updateAverageRating(updatedReviews);
+    } catch(error) {
+      console.log(error)
+    } 
+  }
+
+  const updateAverageRating = (updatedReviews) => {
+    if(updatedReviews.length > 0) {
+      // レビューの星の数の合計値を計算
+      const totalRating = updatedReviews.reduce((acc, review) => acc + review.rating, 0);
+      const average = (totalRating / updatedReviews.length).toFixed(1);
+      console.log(average)
+    }
+
+  }
+  useEffect(() => {
+      const fetchReviews = async() => {
+          try {
+            const response = await laravelAxios.get(`api/reviews/${media_type}/${media_id}`);
+            setReviews(response.data);
+          }catch(error){
+              console.log(error);
+          }
+      }
+      fetchReviews();
+
+  },[media_type,media_id])
+
   return (
     <AppLayout
     header={
@@ -17,6 +86,7 @@ const detail = ({ detail, media_type }) => {
         <title>Detail</title>
     </Head>
 
+    {/* 映画情報部分 */}
     <Box
         sx={{
           height: {xs: "auto", md: "70vh"},
@@ -57,6 +127,29 @@ const detail = ({ detail, media_type }) => {
             <Grid item xs={8} md={8}>
               <Typography variant="h4" paragraph>{detail.title || detail.name}</Typography>
               <Typography paragraph>{detail.overview}</Typography>
+              <Box
+                gap={2}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  mb:2
+                }}
+              >
+                <Rating
+                  readOnly
+                  precision={0.5}
+                  emptyIcon={<StarIcon style={{color: "white"}}/>}
+                />
+                <Typography
+                  sx={{
+                    ml:1,
+                    fontSize: '1.5rem',
+                    fontWeight: "bold"
+                  }}
+                >
+                  3
+                </Typography>
+              </Box>
               <Typography variant='h6'>
                   {media_type == "movie" ? `公開日：${detail.release_date}` : `初回放送日：${detail.first_air_date}`}
               </Typography>
@@ -64,7 +157,116 @@ const detail = ({ detail, media_type }) => {
           </Grid>
         </Container>
     </Box>
+    {/* 映画情報ここまで */}
+
+    {/* レビュー内容表示 */}
+    <Container sx={{py:4}}>
+          <Typography
+              component={"h1"}
+              variant='h4'
+              align='center'
+              gutterBottom
+          >
+            レビュー一覧
+          </Typography>
+
+          <Grid container spacing={3}>
+              {reviews.map((review) => (
+                  <Grid item xs={12} key={review.id}>
+                        <Card>
+                            <CardContent>
+                                <Typography
+                                    variant='h6'
+                                    component={"div"}
+                                    gutterBottom
+                                >
+                                    {review.user.name}
+                                </Typography>
+
+                                <Rating
+                                    value={review.rating}
+                                    readOnly
+                                />
+
+                                <Typography
+                                    variant='body2'
+                                    color="textSeconfary"
+                                    paragraph
+                                >
+                                    {review.content}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                  </Grid>
+              ))}
+          </Grid>
+    </Container>
+    {/* レビュー内容表示ここまで */}
     
+    {/* レビュー追加ボタン */}
+    <Box
+        sx={{
+            position: "fixed",
+            bottom: "16px",
+            right: "16px",
+            zIndex: 5,
+        }}
+    >
+        <Tooltip title="レビュー追加">
+            <Fab
+                  style={{background: "#1976d2", color: "white"}}
+                  onClick={handleOpen}
+                >
+                  <AddIcon />
+            </Fab>
+        </Tooltip>
+    </Box>
+    {/* レビュー追加ボタンここまで */}
+
+    {/* モーダルウィンドウ */}
+    <Modal open={open} onClose={handleClose}>
+          <Box
+              sx={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  width: 400,
+                  bgcolor: "background.paper",
+                  border: "2px solid, #000",
+                  boxShadow: 24,
+                  p: 8,
+              }}
+          >
+              <Typography variant='h6' component="h2">
+                    レビューを書く
+              </Typography>
+
+              <Rating
+                  required
+                  onChange={handleRatingChange}
+                  value={rating}
+              />
+              <TextareaAutosize
+                  required
+                  minRows={5}
+                  placeholder="レビュー内容"
+                  style={{ width: "100px", marginTop: "10px" }}
+                  onChange={handleReviewChange}
+                  value={review}
+              />
+
+              <Button
+                  variant='outlined'
+                  disabled={isDisabled}
+                  onClick={handleReviewAdd}
+              >
+                    送信
+              </Button>
+          </Box>
+
+    </Modal>
+
     </AppLayout>
   )
 }
---
+++ src/server/app/Http/Controllers/ReviewController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Review;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redis;
+
+class ReviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index($media_type, $media_id)
+    {
+        $reviews = Review::with('user')
+            ->where('media_type', $media_type)
+            ->where('media_id', $media_id)
+            ->get();
+
+        return response()->json($reviews);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            "content" => 'required|string',
+            "rating" => 'required|integer',
+            "media_type" => 'required|string',
+            "media_id" => 'required|integer',
+        ]);
+
+        $review = Review::create([
+            "user_id" => 1,
+            "content" => $validateData["content"],
+            "rating" => $validateData["rating"],
+            "media_type" => $validateData["media_type"],
+            "media_id" => $validateData["media_id"],
+        ]);
+
+        $review->load('user');
+        return response()->json($review);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Review $review)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Review $review)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Review $review)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Review $review)
+    {
+        //
+    }
+}
